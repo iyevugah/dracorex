2dc101c0e71f153e36b6d7d1ba2b86ce8ddde9e0
iyevugah <iyevugah@icloud.com>
2022-10-04 18:35:46 -0500

    update of header files to include HouLux creep models

---
+++ include/material/EpsKHouLux.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "RadialReturnStressUpdate.h"
+
+/**
+* This class uses the stress update material in a radial return isotropic creep
+* model.  This class computes the primary creep component of the modified Lubby2 creep.
+ */
+
+template <bool is_ad>
+class EpsKHouLuxTempl : public RadialReturnStressUpdateTempl<is_ad>
+{
+public:
+  static InputParameters validParams();
+
+  EpsKHouLuxTempl(const InputParameters & parameters);
+
+  using Material::_qp;
+  using Material::_dt;
+  using RadialReturnStressUpdateTempl<is_ad>::_base_name;
+  using RadialReturnStressUpdateTempl<is_ad>::_three_shear_modulus;
+
+protected:
+  virtual void initQpStatefulProperties() override;
+  virtual void propagateQpStatefulProperties() override;
+
+  virtual void
+  computeStressInitialize(const GenericReal<is_ad> & effective_trial_stress,
+                          const GenericRankFourTensor<is_ad> & elasticity_tensor) override;
+  virtual GenericReal<is_ad> computeResidual(const GenericReal<is_ad> & effective_trial_stress,
+                                             const GenericReal<is_ad> & scalar) override;
+  virtual GenericReal<is_ad> computeDerivative(const GenericReal<is_ad> & effective_trial_stress,
+                                               const GenericReal<is_ad> & scalar) override;
+  virtual void
+  computeStressFinalize(const GenericRankTwoTensor<is_ad> & creep_strain_increment) override;
+
+
+  /// plastic strain in this model
+  GenericMaterialProperty<RankTwoTensor, is_ad> & _creep_strain;
+
+  /// old value of plastic strain
+  const MaterialProperty<RankTwoTensor> & _creep_strain_old;
+
+  /// plastic strain in this model
+  GenericMaterialProperty<Real, is_ad> & _effective_creep_increment;
+
+  /// old value of plastic strain
+  const MaterialProperty<Real> & _effective_creep_increment_old;
+
+  /// Kelvin ViscoParameter
+  const Real _mvK;
+
+  /// Kelvin Elastic Parameter
+  const Real _mk;
+
+  /// Initial Kelvin Viscosity
+  const Real _etaK0;
+
+  /// Initial Kelvin Shear Modulus
+  const Real _GK0;
+
+  ///@{ Material property that provides the damage index
+  const GenericMaterialProperty<Real, is_ad> & _damage_property;
+
+};
+
+typedef EpsKHouLuxTempl<false> EpsKHouLux;
+typedef EpsKHouLuxTempl<true> ADEpsKHouLux;
---
+++ include/material/EpsMHouLux.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "RadialReturnStressUpdate.h"
+
+/**
+* This class uses the stress update material in a radial return isotropic creep
+* model.  This class computes the primary creep component of the modified Lubby2 creep.
+ */
+
+template <bool is_ad>
+class EpsMHouLuxTempl : public RadialReturnStressUpdateTempl<is_ad>
+{
+public:
+  static InputParameters validParams();
+
+  EpsMHouLuxTempl(const InputParameters & parameters);
+
+  using Material::_qp;
+  using Material::_dt;
+  using RadialReturnStressUpdateTempl<is_ad>::_base_name;
+  using RadialReturnStressUpdateTempl<is_ad>::_three_shear_modulus;
+
+protected:
+  virtual void initQpStatefulProperties() override;
+  virtual void propagateQpStatefulProperties() override;
+
+  virtual void
+  computeStressInitialize(const GenericReal<is_ad> & effective_trial_stress,
+                          const GenericRankFourTensor<is_ad> & elasticity_tensor) override;
+  virtual GenericReal<is_ad> computeResidual(const GenericReal<is_ad> & effective_trial_stress,
+                                             const GenericReal<is_ad> & scalar) override;
+  virtual GenericReal<is_ad> computeDerivative(const GenericReal<is_ad> & effective_trial_stress,
+                                               const GenericReal<is_ad> & scalar) override;
+  virtual void
+  computeStressFinalize(const GenericRankTwoTensor<is_ad> & plastic_strain_increment) override;
+
+
+  /// plastic strain in this model
+  GenericMaterialProperty<RankTwoTensor, is_ad> & _plastic_strain;
+
+  /// old value of plastic strain
+  const MaterialProperty<RankTwoTensor> & _plastic_strain_old;
+
+  /// Maxwell viscosity parameter
+  const Real _mvM;
+
+  /// Maxwell initial viscosity
+  const Real _etaM0;
+
+  ///@{ Material property that provides the damage index
+  const GenericMaterialProperty<Real, is_ad> & _damage_property;
+
+};
+
+typedef EpsMHouLuxTempl<false> EpsMHouLux;
+typedef EpsMHouLuxTempl<true> ADEpsMHouLux;
